4532e935a261f725bfb316746a198b586b36bca4
PaulCreaney <paulcreaney0@gmail.com>
2024-10-13 02:10:40 -0300

    Screen de categories, faltan los espaciados entre cards

--- app/src/main/java/ar/com/be_tp3_g4/ui/screens/Categories.kt
+++ app/src/main/java/ar/com/be_tp3_g4/ui/screens/Categories.kt
@@ -2,11 +2,9 @@ package ar.com.be_tp3_g4.ui.screens
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,42 +17,39 @@ import ar.com.be_tp3_g4.ui.components.Card
 import ar.com.be_tp3_g4.ui.theme.BE_TP3_G4Theme
 
 @Composable
-fun CatalogScreen() {
+fun Categories() {
     val products = listOf(
-        Producto("Pescado", 11.2, "hola", "1kg, Price"),
-        Producto("Carne", 15.5, "hola", "1kg, Price"),
-        Producto("Pollo", 8.3, "hola", "1kg, Price"),
-        Producto("Verduras", 5.0, "hola", "1kg, Price"),
-        Producto("Frutas", 7.5, "hola", "1kg, Price"),
-        Producto("Lácteos", 3.2, "hola", "1kg, Price")
+        Producto("Diet Coke", 1.99, "hola", "355ml, Price"),
+        Producto("Sprite Can", 1.50, "hola", "325ml, Price"),
+        Producto("Apple & Grape ", 15.99, "hola", "2L, Price"),
+        Producto("Orange Juice", 15.99, "hola", "2L, Price"),
+        Producto("Coca Cola Can", 4.99, "hola", "325ml, Price"),
+        Producto("Pepsi Can", 4.99, "hola", "330ml, Price")
     )
-
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp)
-    ) {
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 90.dp), //Bajar la lista
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
+    )  {
         items(products) { product ->
             Card(
-                goToDetails = { /* Navegar a detalles */ },
+                goToDetails = { /* Detalles? */ },
                 producto = product,
-                addToCart = { /* Agregar al carrito */ },
+                addToCart = { /* Agregar al carrito? */ },
                 modifier = Modifier
-                    .padding(8.dp) // Espacio entre tarjetas
-                    .width(150.dp) // Ajusta el ancho de la tarjeta solo aquí
-                    .height(200.dp) // Ajusta la altura de la tarjeta solo aquí
+                    .padding(8.dp)
+                    .width(150.dp)
+                    .height(200.dp)
             )
         }
     }
 }
-
-
-
-
 @Preview(showBackground = true)
 @Composable
-fun PreviewCatalogScreen() {
+fun PreviewCategories() {
     BE_TP3_G4Theme {
-        CatalogScreen()
+        Categories()
     }
 }
